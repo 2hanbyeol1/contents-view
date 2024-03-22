978e3e8aef7496857d1fd64e682a47cceb8e07b4
2hanbyeol1 <2hanbyeol1@naver.com>
2024-03-23 05:09:43 +0900

    fix: getDDay 로직

--- src/hooks/useSetBannerStatus.ts
+++ src/hooks/useSetBannerStatus.ts
@@ -15,24 +15,37 @@ const useSetBannerStatus = (
     date: [Date, Date],
     bannerStateRef: RefObject<HTMLSpanElement>
   ) => {
-    const now = new Date().getTime();
+    const now = new Date();
+    const currentTime = now.getTime();
     const begin = date[0].getTime();
     const end = date[1].getTime();
 
-    if (bannerStateRef.current)
-      if (begin <= now && now <= end) {
-        setStatus('진행 중');
-        bannerStateRef.current.style.backgroundColor = 'rgb(252, 91, 168)';
-      } else if (now > end) {
+    if (bannerStateRef.current) {
+      if (currentTime > end) {
         setStatus('종료');
         bannerStateRef.current.style.backgroundColor = 'rgb(0, 0, 0)';
-      } else {
-        setStatus(`D-${getDDay(begin - now)}`);
-        bannerStateRef.current.style.backgroundColor = 'rgb(255, 48, 76)';
+        return;
       }
+      const DDay = getDDay(date[0], now);
+      if (DDay === 0 || (begin <= currentTime && currentTime <= end)) {
+        setStatus('진행 중');
+        bannerStateRef.current.style.backgroundColor = 'rgb(252, 91, 168)';
+        return;
+      }
+      if (true) {
+        setStatus(`D-${DDay}`);
+        bannerStateRef.current.style.backgroundColor = 'rgb(255, 48, 76)';
+        return;
+      }
+    }
   };
 
-  const getDDay = (timeDiff: number): number => {
+  const getDDay = (begin: Date, now: Date) => {
+    const beginDate = begin;
+    beginDate.setHours(0, 0, 0, 0);
+    now.setHours(0, 0, 0, 0);
+    const timeDiff = beginDate.getTime() - now.getTime();
+
     return Math.ceil(timeDiff / (1000 * 3600 * 24)); // 밀리초를 일로 변환
   };
 
