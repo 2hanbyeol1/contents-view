10eacae2880f7391229fa4e98b8b33425cd1063b
2hanbyeol1 <2hanbyeol1@naver.com>
2024-03-24 07:49:24 +0900

    feat: infinite scroll event

--- src/components/event/eventList/EventListSection.tsx
+++ src/components/event/eventList/EventListSection.tsx
@@ -2,13 +2,27 @@ import './EventListSection.scss';
 import EventBanner from './EventBanner';
 
 import { EVENTS } from '../../../data/event';
+import useInfiniteScroll from '../../../hooks/useInfiniteScroll';
+import Loading from '../../common/Loading';
 
 const EventListSection = () => {
+  const dataPerPage = 3;
+  let page = 0;
+
+  const getNewItems = () => {
+    const newItems = EVENTS.slice(page * dataPerPage, (page + 1) * dataPerPage);
+    page += 1;
+    return newItems;
+  };
+
+  const { items, targetRef, isLoading } = useInfiniteScroll(getNewItems);
+
   return (
     <section className="event-list-section">
-      {EVENTS.map((event, idx) => (
+      {items.map((event, idx) => (
         <EventBanner key={`event-${idx}`} event={event} />
       ))}
+      <Loading targetRef={targetRef} isLoading={isLoading} />
     </section>
   );
 };
