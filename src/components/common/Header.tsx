9b30b257357d00085784388d354e125125458bda
2hanbyeol1 <2hanbyeol1@naver.com>
2024-03-23 21:39:53 +0900

    fix: num of slides not enough 경고 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -9,6 +9,10 @@ import { HEADER_ITEMS } from '../../constants/header';
 const Header = () => {
   const location = useLocation();
 
+  const headerItemArr = Object.entries(HEADER_ITEMS).filter(
+    (item) => location.pathname !== item[0]
+  );
+
   return (
     <header className="header">
       <div className="header-current">
@@ -18,22 +22,20 @@ const Header = () => {
         loop={true}
         spaceBetween={0}
         centeredSlides={true}
-        slidesPerView={4}
+        slidesPerView={5}
         className="header-swiper"
       >
-        {Object.entries(HEADER_ITEMS)
-          .filter((item) => location.pathname !== item[0])
-          .map((item) => {
-            const path = item[0];
-            const name = item[1];
-            return (
-              <SwiperSlide key={path}>
-                <Link className={'header-item'} to={path}>
-                  {name}
-                </Link>
-              </SwiperSlide>
-            );
-          })}
+        {[...headerItemArr, ...headerItemArr].map((item, idx) => {
+          const path = item[0];
+          const name = item[1];
+          return (
+            <SwiperSlide key={`header-${idx}`}>
+              <Link className={'header-item'} to={path}>
+                {name}
+              </Link>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </header>
   );
